acfb53941052f70457dd262aff52c423a629762a
Claravetrano <claravetrano1@gmail.com>
2024-07-07 19:40:35 -0300

    database.py

--- database.py
+++ database.py
@@ -235,11 +235,7 @@ def verCitasProf(nombre_prof):
 #     cursor.execute("SELECT userid, contraseña, nombre, servicios, precio FROM profesionales")
 #     profesionales_data = cursor.fetchall()
 #     conn.close()
-#   ta:
-#         if len(data) == 5:
-#             try:
-#                 user_id, contraseña, nombre, servicio, precio = data
-#                 profesional = Profesional(user_id, contraseña, nombre, servicio, precio)
+
 
 
 
